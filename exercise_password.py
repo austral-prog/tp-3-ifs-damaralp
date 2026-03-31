5d121fd94378af65bc63efe23719974635231611
damaralp <damaralpaz@mail.austral.edu.ar>
2026-03-31 10:44:07 -0300

    Update exercise_password.py

--- exercise_password.py
+++ exercise_password.py
@@ -24,3 +24,38 @@ def password():
         Debe contener un numero
     """
     pass
+
+    contrasena = input()
+    largo = False
+    numero = False
+
+    if len(contrasena) >= 8:
+        largo = True
+
+    if "0" in contrasena:
+        numero = True
+    if "1" in contrasena:
+        numero = True
+    if "2" in contrasena:
+        numero = True
+    if "3" in contrasena:
+        numero = True
+    if "4" in contrasena:
+        numero = True
+    if "5" in contrasena:
+        numero = True
+    if "6" in contrasena:
+        numero = True
+    if "7" in contrasena:
+        numero = True
+    if "8" in contrasena:
+        numero = True
+    if "9" in contrasena:
+        numero = True
+
+    if largo == False:
+        print("Contraseña muy corta")
+    if numero == False:
+        print("Debe contener un numero")
+    if largo == True and numero == True:
+        print("Contraseña valida")
